82e7c5f8d9dffcd4164095c424fbfc347b6c22d1
Ossi Heino <ossi.heino@gmail.com>
2021-10-27 00:17:17 +0300

    restapi started

---
+++ backend/api/config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    PORT: 1337,
+    DBURI: 'mongodb://localhost:27017/testi'
+}
---
+++ backend/api/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import config from 'config';
+import connect from './utils/connect';
+import routes from './routes';
+
+const port = config.get<number>('PORT');
+const app = express();
+
+app.listen(port, async () => {
+    console.log("Listening...");
+    //await connect();
+    routes(app);
+});
