18ea0c88a1cf209889ec497b8a1a008319863b0d
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-10-29 16:01:29 +0300

    Routes for frontend added
    -Frontend is divided in components
    -Routes show different components in homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
 import './App.css';
-import GetCustomers from './components/Customers';
+//Components
+import Homepage from './components/Homepage';
+import Navigation from './components/Navigation';
+import Customerlist from './components/Customerlist';
+import Invoice from './components/Invoice';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Invoice App
-        </h1>
-        <p>
-          This is invoice-app
-        </p>
-        <p>
-        <GetCustomers/>
-        </p>
-      </header>      
+    <Router>
+      <div className="App">
+        <Navigation/>
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/customers" component={Customerlist} />
+          <Route path="/invoice" component={Invoice} />
+        </Switch>    
     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Customerlist.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function Customerlist() {
+    return(
+        <div className="customerList">
+            <button>10 edellistä</button>
+            <h1>Lista asiakkaista tähän</h1>
+            <button>10 seuraavaa</button>
+            <button>Valitse</button>
+        </div>
+    )
+}
+
+export default Customerlist
