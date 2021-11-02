d110abc2b47e8ff69cba2ff8beb5b0e639fc3f7c
Teemu Leskinen <t8lete03@students.oamk.fi>
2021-11-02 10:27:14 +0200

    Customer adding created

--- frontend/src/components/Addcustomer.tsx
+++ frontend/src/components/Addcustomer.tsx
@@ -1,15 +1,105 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-function Addcustomer() {
+interface ICustomer {
+        _id?: string,
+        YTunnus: string,
+        asiakkaanNimi: string,
+        Postitusosoite: string,
+        Postinumero: string,
+        Toimipaikka: string
+}
+
+const Addcustomer = () => {
+    const [input, setInput] = useState<ICustomer>({
+        YTunnus: "",
+        asiakkaanNimi: "",
+        Postitusosoite: "",
+        Postinumero: "",
+        Toimipaikka: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (): void =>{
+        fetch('http://localhost:8040/customer', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json'},
+            body: JSON.stringify({
+                YTunnus: input.YTunnus,
+                asiakkaanNimi: input.asiakkaanNimi,
+                Postitusosoite: input.Postitusosoite,
+                Postinumero: input.Postinumero,
+                Toimipaikka: input.Toimipaikka
+            })
+        })
+        .then(function(data){
+            console.log("Request succeeded with response ", data)            
+        })
+        .catch(function(error){
+            console.log("Request failed ", error)
+        })
+        setInput({
+            YTunnus: "",
+            asiakkaanNimi: "",
+            Postitusosoite: "",
+            Postinumero: "",
+            Toimipaikka: ""
+        })
+    }
+
     return(
-        <div className="addCustomer">
-            <form>
-                <h3>Lisää asiakas</h3>
-                <input type="text" className="inputtesti" />
-                <button className = "formButton" >
-                    Testibutton
+        <div className="AddCustomer">
+            <h3>Lisää asiakas</h3>
+                <input 
+                    type="text"
+                    placeholder="Y-Tunnus"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="YTunnus"
+                    value={input.YTunnus}
+                />
+                <input 
+                    type="text"
+                    placeholder="Yrityksen nimi"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="asiakkaanNimi"
+                    value={input.asiakkaanNimi}
+                />
+                <input 
+                    type="text"
+                    placeholder="Osoite"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Postitusosoite"
+                    value={input.Postitusosoite}
+                />
+                <input 
+                    type="text"
+                    placeholder="Postinumero"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Postinumero"
+                    value={input.Postinumero}
+                />
+                <input 
+                    type="string"
+                    placeholder="Kaupunki"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Toimipaikka"
+                    value={input.Toimipaikka}
+                />            
+            <button 
+                className = "AddCustomer-btn"
+                onClick={handleClick}>
+                Lisää asiakas
             </button>
-        </form>
         </div>
     )
 }
