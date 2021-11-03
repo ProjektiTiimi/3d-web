5cf907da15fc5d44e85c40da12ca03f66af18cd0
Arttu Rusanen <54593874+ArtRus97@users.noreply.github.com>
2021-11-03 12:01:31 +0200

    Initialized Customer editing

--- frontend/src/components/Editcustomer.tsx
+++ frontend/src/components/Editcustomer.tsx
@@ -1,23 +1,98 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import Customer from '../models/customer';
 
-function Editcustomer() {
+
+const Editcustomer = () => {
+    const [input, setInput] = useState<Customer>({
+        YTunnus: "",
+        asiakkaanNimi: "",
+        Postitusosoite: "",
+        Postinumero: "",
+        Toimipaikka: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (): void =>{
+        fetch('http://localhost:1337/customer', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json'},
+            body: JSON.stringify({
+                YTunnus: input.YTunnus,
+                asiakkaanNimi: input.asiakkaanNimi,
+                Postitusosoite: input.Postitusosoite,
+                Postinumero: input.Postinumero,
+                Toimipaikka: input.Toimipaikka
+            })
+        })
+        .then(function(data){
+            console.log("Request succeeded with response ", data)            
+        })
+        .catch(function(error){
+            console.log("Request failed ", error)
+        })
+        setInput({
+            YTunnus: "",
+            asiakkaanNimi: "",
+            Postitusosoite: "",
+            Postinumero: "",
+            Toimipaikka: ""
+        })
+    }
+
     return(
-        <div className="editCustomer">
-            <form>
-                <div className="editCustomer">
-                    <h3>Muokkaa asiakasta</h3>
-                </div>
-                <input type="text" className="editCustomerInput" />
-                <button className = "editButton" >
-                    muokkaa
-                </button>
-                <button className = "deleteButton" >
-                    poista
-                </button>
-                <button className = "saveButton" >
-                    tallenna
-                </button>
-            </form>
+        <div className="AddCustomer">
+            <h3>Muokkaa asiakasta</h3>
+                <input 
+                    type="text"
+                    placeholder="Y-Tunnus"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="YTunnus"
+                    value={input.YTunnus}
+                />
+                <input 
+                    type="text"
+                    placeholder="Yrityksen nimi"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="asiakkaanNimi"
+                    value={input.asiakkaanNimi}
+                />
+                <input 
+                    type="text"
+                    placeholder="Osoite"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Postitusosoite"
+                    value={input.Postitusosoite}
+                />
+                <input 
+                    type="text"
+                    placeholder="Postinumero"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Postinumero"
+                    value={input.Postinumero}
+                />
+                <input 
+                    type="string"
+                    placeholder="Kaupunki"
+                    className="AddCustomer-input"
+                    onChange={handleChange}
+                    name="Toimipaikka"
+                    value={input.Toimipaikka}
+                />            
+            <button 
+                className = "AddCustomer-btn"
+                onClick={handleClick}>
+                Tallenna
+            </button>
         </div>
     )
 }
