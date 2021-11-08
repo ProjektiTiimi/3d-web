c342689a00f94bf7a179a6919ac1b2274da399bf
Arttu Rusanen <54593874+ArtRus97@users.noreply.github.com>
2021-11-08 11:49:58 +0200

    Update customer.ts

--- backend/api/src/models/customer.ts
+++ backend/api/src/models/customer.ts
@@ -6,14 +6,20 @@ export interface ICustomer extends mongoose.Document{
     Postitusosoite: String;
     Postinumero: String; 
     Toimipaikka: String;
+    Puhelin: String;
+    Kotisivu: String;
+    Sahkoposti: String;
 }
 
 export const CustomerSchema = new mongoose.Schema({
-    YTunnus: { type: String, required: true},
-    asiakkaanNimi: { type: String, required: true},
-    Postitusosoite: { type: String },
-    Postinumero: { type: String },
-    Toimipaikka: {  type: String }
+    YTunnus: { type: String },
+    asiakkaanNimi: { type: String, required: true },
+    Postitusosoite: { type: String, required: true },
+    Postinumero: { type: String, required: true },
+    Toimipaikka: {  type: String, required: true },
+    Puhelin: {  type: String },
+    Kotisivu: {  type: String },
+    Sahkoposti: {  type: String }
 })
 
 const Customer = mongoose.model<ICustomer>("Customer", CustomerSchema);
