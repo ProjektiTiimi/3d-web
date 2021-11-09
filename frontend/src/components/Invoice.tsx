d74a3744d9513c1c710230c754f95026f4edff26
TeemuLeskinen <54636537+TeemuLeskinen@users.noreply.github.com>
2021-11-09 11:41:24 +0200

    Base for the invoice info

--- frontend/src/components/Invoice.tsx
+++ frontend/src/components/Invoice.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Customerlist from "./Customerlist";
+import Invoiceinfo from "./Invoiceinfo";
 
 function Invoice() {
     return(
         <div className="App">
             <Customerlist />
+            <Invoiceinfo />
+            <div className="AddCustomer">
+                <form>
+                    <h3>lasku</h3>
+                    <input 
+                        type="text"
+                        placeholder="Tilinumero"
+                        className="Invoice-input"
+                        name="Tilinumero"
+                        value="Testikenttä"
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Laskun numero"
+                        className="Invoice-input"
+                        name="laskunNumero"
+                        value="Testikenttä 2"
+                    />
+                </form>
+            </div>
             
         </div>
     )
