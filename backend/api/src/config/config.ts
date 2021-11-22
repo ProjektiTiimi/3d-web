9d68c6d73b32538bddbc40203178d64147edb1dc
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-11-22 19:45:47 +0200

    added auth for database

--- backend/api/src/config/config.ts
+++ backend/api/src/config/config.ts
@@ -1,4 +1,5 @@
 export const PORT = 1337;
-export const DBURI = 'mongodb://127.0.0.1:27017/UusiDB';
+export const DBURI = 'mongodb://admin:pass@127.0.0.1:27019/UusiDB?authSource=admin';
+//export const DBURI = 'mongodb://admin:pass@3d-web-mongo-1:27017/UusiDB?authSource=admin';
 export const SECRETKEY = 'dsdf22-232sfg-666d34-123fgh'
 
