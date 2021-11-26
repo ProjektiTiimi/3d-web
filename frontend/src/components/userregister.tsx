c7030487f9bb3eb906053c5d348e47b48682cd9a
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-11-26 10:37:27 +0200

    added validation for user register
    Inputs cannot be left empty

--- frontend/src/components/userregister.tsx
+++ frontend/src/components/userregister.tsx
@@ -8,7 +8,12 @@ export function RegisterUser(){
         password:'',
         ytunnus:'',
         email:'',
-        tilinumero:''
+        tilinumero:'',
+        usernameError: "",
+        passwordError: "",
+        ytunnusError: "",
+        emailError: "",
+        tilinumeroError: ""
     })
     const handleChange = (e: { target: { name: any; value: any; }; }) =>{
         const {name, value} = e.target 
@@ -17,14 +22,52 @@ export function RegisterUser(){
     })
     }
 
+
+    const validate = () => {
+        let usernameError = "";
+        let passwordError = "";
+        let ytunnusError = "";
+        let emailError = "";
+        let tilinumeroError = "";
+        if (!user.username) {
+            usernameError = "Tunnus ei voi olla tyhjä";         
+        }
+        if (!user.password) {
+            passwordError = "Salasana ei voi olla tyhjä";
+        }
+        if (!user.ytunnus) {
+            ytunnusError = "Y-tunnus ei voi olla tyhjä";
+        }
+        if (!user.email) {
+            emailError = "Email ei voi olla tyhjä";
+        }
+        if (!user.tilinumero) {
+            tilinumeroError = "Tilinumero ei voi olla tyhjä";
+        }
+        if (usernameError || passwordError || ytunnusError || emailError || tilinumeroError) {
+            setUser({...user, usernameError, passwordError, ytunnusError, emailError, tilinumeroError})
+            return false;
+        }
+        return true;
+    };
+
     const Register = () =>{
-        const {username, password, ytunnus, email, tilinumero} = user
-        if (username && password){
+        const isValid = validate();
+        if(isValid){
             axios.post(`${configData.API_URL}:${configData.API_PORT}/user/register`, user)
             .then(res=>console.log(res));
-        }
-        else{
-            alert("Invalid username or password");
+            setUser({
+                username:'',
+                password:'',
+                ytunnus:'',
+                email:'',
+                tilinumero:'',
+                usernameError: "",
+                passwordError: "",
+                ytunnusError: "",
+                emailError: "",
+                tilinumeroError: ""
+            })
         }
     }
 
@@ -32,10 +75,15 @@ export function RegisterUser(){
         <div className="AddCustomer">
         <h1>Rekisteröidy</h1>
         Käyttäjätunnus: <input type="text" value={user.username} name="username" onChange={handleChange} className="AddCustomer-input"></input>
+        <div className="errorMessage">{user.usernameError}</div>
         Salasana: <input type="password" value={user.password} name="password" onChange={handleChange} className="AddCustomer-input"></input>
+        <div className="errorMessage">{user.passwordError}</div>
         Y-tunnus: <input type="text" value={user.ytunnus} name="ytunnus" onChange={handleChange} className="AddCustomer-input"></input>
+        <div className="errorMessage">{user.ytunnusError}</div>
         E-mail: <input type="text" value={user.email} name="email" onChange={handleChange} className="AddCustomer-input"></input>
+        <div className="errorMessage">{user.emailError}</div>
         Tilinumero: <input type="text" value={user.tilinumero} name="tilinumero" onChange={handleChange} className="AddCustomer-input"></input>
+        <div className="errorMessage">{user.tilinumeroError}</div>
         <input type="submit" onClick={Register} className="AddCustomer-btn"></input>
         </div>
     )
